3d701a5bf343d696b0bb656c3a18257d1d81d24c
Olivia247 <56613939+Olivia247@users.noreply.github.com>
2022-05-18 22:31:26 -0500

    Create caps.js

---
+++ caps.js
@@ -0,0 +1,51 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// right side
+gsap.to("#content1", {
+  scrollTrigger: {
+    trigger: "#content1",
+    toggleActions: "resume pause resume reset",
+  },
+  xPercent: -190,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content2", {
+  scrollTrigger: {
+    trigger: "#content2",
+    toggleActions: "resume pause resume reset",
+  },
+  xPercent: -190,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content3", {
+  scrollTrigger: {
+    trigger: "#content3",
+    toggleActions: "resume pause resume reset",
+  },
+  xPercent: -190,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content4", {
+  scrollTrigger: {
+    trigger: "#content4",
+    toggleActions: "resume pause resume reset",
+  },
+  xPercent: -190,
+  duration: 1.4,
+});
+
+// right side
+gsap.to("#content5", {
+  scrollTrigger: {
+    trigger: "#content5",
+    toggleActions: "resume pause resume reset",
+  },
+  xPercent: -190,
+  duration: 1.4,
+});
